223a0ffcd6728e2308f143a5e749b816f29afd7e
devlex <48767282+DevAlexI@users.noreply.github.com>
2021-09-01 21:04:44 -0500

    Commit actions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+  res.end('Hola Mundo Alex');
 });
 
 server.listen(port, hostname, () => {
